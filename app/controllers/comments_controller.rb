4416114a72d75964ee485389baaafa19a39524de
javierc22 <javier.ec.22@gmail.com>
2017-12-04 23:26:29 -0300

    agregando raty-js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -57,6 +57,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content, :user_id, :cook_id)
+      params.require(:comment).permit(:content, :rating, :user_id, :cook_id)
     end
 end
