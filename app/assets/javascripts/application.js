d3d6c88f5121b7817cb28e4168a9d87d4e658824
javierc22 <javier.ec.22@gmail.com>
2017-12-08 13:22:50 -0300

    Edición datos de usuario

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,9 +20,6 @@
 
 //= require jquery/dist/jquery
 //=require raty-js/lib/jquery.raty
-//= require tether/dist/js/tether.js
-//= require moment/moment.js
-//= require bootstrap/dist/js/bootstrap.js
 //= require bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min
 //= require moment-timezone/builds/moment-timezone.min
 
