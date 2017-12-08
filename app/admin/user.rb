a0181ef5df803e4d97cff9bafe272c37c1a222bb
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2017-12-08 17:23:39 -0300

    admin recurso user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -11,6 +11,7 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  permit_params :name, :phone, :cook, :role
   index do
     column :email
     column :name
@@ -22,6 +23,33 @@ ActiveAdmin.register User do
     column :member_since do |user|
       time_ago_in_words(user.created_at)
     end
+    actions
   end
+
+  form do |f|
+    inputs 'User' do
+      input :email, input_html: { readonly: true, disabled: true  }, as: :string if !f.object.new_record?
+      input :name
+      input :phone
+      input :cook
+      input :role
+      input :password
+    end
+    actions
+  end
+
+  controller do
+   def update
+     if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
+       params[:user].delete("password")
+       params[:user].delete("password_confirmation")
+     end
+     super
+   end
+  end
+
+  filter :email, as: :select
+  filter :cook
+  filter :role
 
 end
